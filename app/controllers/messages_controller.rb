27ffb65cf922556f926ed5233042e7332c630645
Salman Ansari <salman.ansari@gmail.com>
2015-01-02 18:20:32 -0800

    fix bundle selector to properly pass SRM bundle ID

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 
   def index
     @messages = Message.fetch_all(bundle_id: params[:bundle_id])
-    @bundles = SrmBundle.all.map { |bundle| [bundle.name, bundle.id] }
+    @bundles = SrmBundle.all.map { |bundle| [bundle.name, bundle.bundle_id] }
   end
 
 end
